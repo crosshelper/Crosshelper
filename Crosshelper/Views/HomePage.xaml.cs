a2c495d62a6b1b1b9ab06b2276a6514527a5b260
bochen <bo.chen@crosshelper.com>
2019-03-26 11:24:44 -0700

    HomePage Done

--- Crosshelper/Views/HomePage.xaml.cs
+++ Crosshelper/Views/HomePage.xaml.cs
@@ -12,7 +12,48 @@ namespace Crosshelper.Views
         public HomePage()
         {
             InitializeComponent();
+            // Lable&text Change
+            // NameId: HomeDate, HomeLanguage, HomeProjectDescription, HomeProjectStatus
 
+        }
+
+        //Top Ring&Menu button
+        void HomeBell(object sender, EventArgs e)
+        {
+            (sender as Button).Text = "Click me again!";
+        }
+        void HomeBurgerMenu(object sender, EventArgs e)
+        {
+            (sender as Button).Text = "Click me again!";
+        }
+
+        //Profile image button
+        void HomeProfileImage(object sender, EventArgs e)
+        {
+            (sender as Button).Text = "Click me again!";
+        }
+
+        //Location&language button
+        void HomeLocation(object sender, EventArgs e)
+        {
+            (sender as Button).Text = "Click me again!";
+        }
+        void HomeLanguage(object sender, EventArgs e)
+        {
+            (sender as Button).Text = "Click me again!";
+        }
+
+        //Get help button
+        void HomeGetHelp(object sender, EventArgs e)
+        {
+            (sender as Button).Text = "Click me again!";
+        }
+
+        //Project view button
+
+        void HomeMore(object sender, EventArgs e)
+        {
+            (sender as Button).Text = "Click me again!";
         }
 
 
