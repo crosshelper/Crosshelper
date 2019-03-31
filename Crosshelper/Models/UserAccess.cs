863ab6458531f5768c7e1849a5a89cd3c6a341dd
WL-CHelper <william.liang@crosshelper.com>
2019-03-31 00:38:53 -0700

    Models

---
+++ Crosshelper/Models/UserAccess.cs
@@ -0,0 +1,85 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Text;
+
+namespace Crosshelper.Models
+{
+    class UserAccess
+    {
+        public UserAccess()
+        {
+
+        }
+
+        string connStr = "server=chdb.cakl0xweapqd.us-west-1.rds.amazonaws.com;port=3306;database=chdb;user=chroot;password=ch123456;charset=utf8";
+
+        public void UserRegister(string Uname, string Pwd)
+        {
+            MySqlConnection conn = new MySqlConnection(connStr);
+            try
+            {
+                if (conn.State == ConnectionState.Closed)
+                {
+                    Console.WriteLine("Connecting to MySQL...");
+                    conn.Open();
+                    Console.WriteLine("Connecting to MySQL...");
+                    conn.Open();
+                    string sql = "INSERT INTO UserMaster(Uid,Uname,Pwd,Permission) VALUES(NULL, @para1, @para2, NULL) ";
+                    MySqlCommand cmd = new MySqlCommand(sql, conn);
+                    cmd.Parameters.AddWithValue("para1", Uname);
+                    cmd.Parameters.AddWithValue("para2", Pwd);
+
+                    cmd.ExecuteNonQuery();
+                    Console.WriteLine("Connecting to MySQL success");
+
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+                Console.WriteLine("Connection failed");
+            }
+            finally
+            {
+                conn.Close();
+            }
+        }
+
+        /// <summary>
+        /// 验证用户名密码，存放回true，否则为false
+        /// </summary>
+        bool VerifyUSser(string username, string password)
+        {
+            
+            //并没有建立数据库连接
+            MySqlConnection conn = new MySqlConnection(connStr);
+            try
+            {   //建立连接，打开数据库
+                conn.Open();
+                string sqlstr = "select * from UserMaster where username = @para1 and password = @para2";
+                MySqlCommand cmd = new MySqlCommand(sqlstr, conn);
+                //通过设置参数的形式给SQL 语句串值
+                cmd.Parameters.AddWithValue("para1", username);
+                cmd.Parameters.AddWithValue("para2", password);
+
+                MySqlDataReader reader = cmd.ExecuteReader();
+                if (reader.Read())
+                {
+                    return true;
+                }
+                return false;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+            finally
+            {
+                conn.Close();   //关闭连接              
+            }
+            return false;
+        }
+    }
+}
--- Crosshelper/Views/HomePage.xaml.cs
+++ Crosshelper/Views/HomePage.xaml.cs
@@ -16,12 +16,13 @@ namespace Crosshelper.Views
             // NameId: HPDate, HPLanguage, HPProjectDescription, HPProjectStatus
             HPProject.GestureRecognizers.Add(new TapGestureRecognizer
             { Command = new Command(() => { HPProject.BackgroundColor = Color.Blue; }) });
+            
         }
 
         //Top Ring&Menu button
         void HPBell(object sender, EventArgs e)
         {
-            (sender as Button).Text = "Click me again!";
+            (sender as Button).Text = "Click me again!"; 
         }
         void HPBurgerMenu(object sender, EventArgs e)
         {
