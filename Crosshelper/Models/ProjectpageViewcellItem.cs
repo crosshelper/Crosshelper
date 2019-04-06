078c5da9bd1109257d63cc2f165ac02d76e7f5d9
bochen <bo.chen@crosshelper.com>
2019-04-06 16:27:44 -0700

    ProjectpageViewcellItem Added

---
+++ Crosshelper/Models/ProjectpageViewcellItem.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+using Xamarin.Forms;
+
+namespace Crosshelper.Models
+{
+    public class ProjectpageViewcellItem : ContentPage
+    {
+        public ProjectpageViewcellItem()
+        {
+        }
+
+        public int PanelID { get; set; }
+        public string Name { get; set; }
+        public int Rating { get; set; }
+        public string Language { get; set; }
+        public string Baseprice { get; set; }
+        public string Location { get; set; }
+        public string ImageUrl { get; set; }
+
+        public override string ToString()
+        {
+            return Language;
+        }
+    }
+}
+
--- Crosshelper/Views/ProjectPage.xaml.cs
+++ Crosshelper/Views/ProjectPage.xaml.cs
@@ -1,15 +1,116 @@
 ﻿using System;
 using System.Collections.Generic;
-
+using Crosshelper.Models;
 using Xamarin.Forms;
 
 namespace Crosshelper.Views
 {
     public partial class ProjectPage : ContentPage
     {
+        public IList<ProjectpageViewcellItem> PanelContent { get; set; }
         public ProjectPage()
         {
             InitializeComponent();
+            PanelContent = new List<ProjectpageViewcellItem>();
+            PanelContent.Add(new ProjectpageViewcellItem
+            {
+                PanelID = 1,
+                Name = "Baboon",
+                Rating = 55555,
+                Language = "Language: Chinese/English",
+                Baseprice = "$40 starting cost",
+                Location = "8 miles away from me",
+                ImageUrl = "http://upload.wikimedia.org/wikipedia/commons/thumb/f/fc/Papio_anubis_%28Serengeti%2C_2009%29.jpg/200px-Papio_anubis_%28Serengeti%2C_2009%29.jpg"
+            });
+
+            PanelContent.Add(new ProjectpageViewcellItem
+            {
+                PanelID = 0,
+                Name = "Mike",
+                Rating = 55555,
+                Language = "Language: Chinese/English",
+                Baseprice = "$40 starting cost",
+                Location = "8 miles away from me",
+                ImageUrl = "http://upload.wikimedia.org/wikipedia/commons/thumb/4/40/Capuchin_Costa_Rica.jpg/200px-Capuchin_Costa_Rica.jpg"
+            });
+            PanelContent = new List<ProjectpageViewcellItem>();
+            PanelContent.Add(new ProjectpageViewcellItem
+            {
+                PanelID = 1,
+                Name = "Baboon",
+                Rating = 55555,
+                Language = "Language: Chinese/English",
+                Baseprice = "$40 starting cost",
+                Location = "8 miles away from me",
+                ImageUrl = "http://upload.wikimedia.org/wikipedia/commons/thumb/f/fc/Papio_anubis_%28Serengeti%2C_2009%29.jpg/200px-Papio_anubis_%28Serengeti%2C_2009%29.jpg"
+            });
+
+            PanelContent.Add(new ProjectpageViewcellItem
+            {
+                PanelID = 0,
+                Name = "Mike",
+                Rating = 55555,
+                Language = "Language: Chinese/English",
+                Baseprice = "$40 starting cost",
+                Location = "8 miles away from me",
+                ImageUrl = "http://upload.wikimedia.org/wikipedia/commons/thumb/4/40/Capuchin_Costa_Rica.jpg/200px-Capuchin_Costa_Rica.jpg"
+            });
+
+            PanelContent.Add(new ProjectpageViewcellItem
+            {
+                PanelID = 0,
+                Name = "David",
+                Rating = 55555,
+                Language = "Language: Japanese/English",
+                Baseprice = "$40 starting cost",
+                Location = "8 miles away from me",
+                ImageUrl = "http://upload.wikimedia.org/wikipedia/commons/thumb/8/83/BlueMonkey.jpg/220px-BlueMonkey.jpg"
+            });
+            PanelContent.Add(new ProjectpageViewcellItem
+            {
+                PanelID = 0,
+                Name = "David",
+                Rating = 55555,
+                Language = "Language: Japanese/English",
+                Baseprice = "$40 starting cost",
+                Location = "8 miles away from me",
+                ImageUrl = "http://upload.wikimedia.org/wikipedia/commons/thumb/8/83/BlueMonkey.jpg/220px-BlueMonkey.jpg"
+            });
+            PanelContent.Add(new ProjectpageViewcellItem
+            {
+                PanelID = 0,
+                Name = "David",
+                Rating = 55555,
+                Language = "Language: Japanese/English",
+                Baseprice = "$40 starting cost",
+                Location = "8 miles away from me",
+                ImageUrl = "http://upload.wikimedia.org/wikipedia/commons/thumb/8/83/BlueMonkey.jpg/220px-BlueMonkey.jpg"
+            });
+            PanelContent.Add(new ProjectpageViewcellItem
+            {
+                PanelID = 0,
+                Name = "David",
+                Rating = 55555,
+                Language = "Language: Japanese/English",
+                Baseprice = "$40 starting cost",
+                Location = "8 miles away from me",
+                ImageUrl = "http://upload.wikimedia.org/wikipedia/commons/thumb/8/83/BlueMonkey.jpg/220px-BlueMonkey.jpg"
+            });
+
+            listView.ItemsSource = PanelContent;
+            listView.BackgroundColor = Color.Transparent;
+            listView.SelectionMode = ListViewSelectionMode.None;
+        }
+
+        void Handle_ItemSelected(object sender, Xamarin.Forms.SelectedItemChangedEventArgs e)
+        {
+            //if (e.SelectedItem == null) return;
+            //((ListView)sender).SelectedItem = null;
+        }
+
+        void DailyLifeButton(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new DescribeProblemPage());
         }
     }
 }
