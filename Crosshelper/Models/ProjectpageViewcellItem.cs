34d37360eebf28e834d5ac2215bef68578394425
bochen <bo.chen@crosshelper.com>
2019-04-06 16:51:33 -0700

    projectpageViewcellItem Done

--- Crosshelper/Models/ProjectpageViewcellItem.cs
+++ Crosshelper/Models/ProjectpageViewcellItem.cs
@@ -12,11 +12,19 @@ namespace Crosshelper.Models
 
         public int PanelID { get; set; }
         public string Name { get; set; }
-        public int Rating { get; set; }
+        public string Date { get; set; }
         public string Language { get; set; }
-        public string Baseprice { get; set; }
-        public string Location { get; set; }
+        public string Status { get; set; }
+        public string Description { get; set; }
+        public string Emergency { get; set; }
+        public int Rating { get; set; }
         public string ImageUrl { get; set; }
+        public string ServiceFee { get; set; }
+        public string EquipmentFee { get; set; }
+        public string ChFee { get; set; }
+        public string Tax { get; set; }
+        public string Total { get; set; }
+        public string Contact { get; set; }
 
         public override string ToString()
         {
