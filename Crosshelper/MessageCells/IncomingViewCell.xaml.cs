23e7744705d8f895f12eb89b2c36f8bb4bca2464
william <william.liang@crosshelper.com>
2019-04-08 01:24:20 -0700

    四月初版

---
+++ Crosshelper/MessageCells/IncomingViewCell.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using Xamarin.Forms;
+
+namespace Crosshelper.MessageCells
+{
+    public partial class IncomingViewCell : ContentView
+    {
+        public IncomingViewCell()
+        {
+            InitializeComponent();
+        }
+    }
+}
