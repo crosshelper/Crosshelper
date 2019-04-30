3f564e257aa6625cd55e53daba91ade569c23e1a
WL-CHelper <william.liang@crosshelper.com>
2019-04-30 01:40:49 -0700

    0

---
+++ Crosshelper/Views/PickerTestPage.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Crosshelper.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class PickerTestPage : ContentPage
+    {
+        public IList Items { get; } = new ObservableCollection<PickerItem>();
+        public ObservableCollection<PickerItem> ItemsSource { get; } = new ObservableCollection<PickerItem>();
+        public ObservableCollection<PickerItem> SelectedItems { get; set; } = new ObservableCollection<PickerItem>();
+
+        public PickerTestPage()
+        {
+            InitializeComponent();
+
+            BindingContext = this;
+
+            Items.Add("abc");
+            Items.Add("def");
+
+            ItemsSource.Add(new PickerItem { Name = "abc" });
+            ItemsSource.Add(new PickerItem { Name = "def" });
+        }
+
+        public class PickerItem
+        {
+            public string Name { get; set; }
+        }
+    }
+}
