e2de84ff5b79274d4bb338a78fc733bad4b9d40f
William Liang <williamliang30@gmail.com>
2019-05-04 19:06:47 -0700

    1

--- Crosshelper/Views/LocationPage.xaml.cs
+++ Crosshelper/Views/LocationPage.xaml.cs
@@ -14,7 +14,102 @@ namespace Crosshelper.Views
         public LocationPage()
         {
             InitializeComponent();
+
+            buttonGetGPS.Clicked += async (sender, args) =>
+            {
+                try
+                {
+                    var locator = CrossGeolocator.Current;
+                    locator.DesiredAccuracy = 1000;
+                    labelGPS.Text = "Getting gps";
+
+                    var position = await locator.GetPositionAsync(timeoutMilliseconds: 10000);
+
+                    if (position == null)
+                    {
+                        labelGPS.Text = "null gps :(";
+                        return;
+                    }
+                    labelGPS.Text = string.Format("Time: {0} \nLat: {1} \nLong: {2} \nAltitude: {3} \nAltitude Accuracy: {4} \nAccuracy: {5} \nHeading: {6} \nSpeed: {7}",
+                        position.Timestamp, position.Latitude, position.Longitude,
+                        position.Altitude, position.AltitudeAccuracy, position.Accuracy, position.Heading, position.Speed);
+
+                }
+                catch(Exception ex)
+                {
+                
+                }
+            };
+
+            buttonTrack.Clicked += async (object sender, EventArgs e) =>
+            {
+                try
+                {
+                    if (CrossGeolocator.Current.IsListening)
+                    {
+                        await CrossGeolocator.Current.StopListeningAsync();
+                        labelGPSTrack.Text = "Stopped tracking";
+                        buttonTrack.Text = "Stop Tracking";
+                    }
+                    else
+                    {
+                        if (await CrossGeolocator.Current.StartListeningAsync(30000, 0))
+                        {
+                            labelGPSTrack.Text = "Started tracking";
+                            buttonTrack.Text = "Track Movements";
+                        }
+                    }
+                }
+                catch(Exception ex)
+                {
+
+                }
+            };
         }
+
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+            try
+            {
+                CrossGeolocator.Current.PositionChanged += CrossGeolocator_Current_PositionChanged;
+                CrossGeolocator.Current.PositionError += CrossGeolocator_Current_PositionError;
+            }
+            catch
+            {
+            }
+        }
+
+        void CrossGeolocator_Current_PositionError(object sender, Plugin.Geolocator.Abstractions.PositionErrorEventArgs e)
+        {
+
+            labelGPSTrack.Text = "Location error: " + e.Error.ToString();
+        }
+
+        void CrossGeolocator_Current_PositionChanged(object sender, Plugin.Geolocator.Abstractions.PositionEventArgs e)
+        {
+            var position = e.Position;
+            labelGPSTrack.Text = string.Format("Time: {0} \nLat: {1} \nLong: {2} \nAltitude: {3} \nAltitude Accuracy: {4} \nAccuracy: {5} \nHeading: {6} \nSpeed: {7}",
+                position.Timestamp, position.Latitude, position.Longitude,
+                position.Altitude, position.AltitudeAccuracy, position.Accuracy, position.Heading, position.Speed);
+
+
+        }
+
+        protected override void OnDisappearing()
+        {
+            base.OnDisappearing();
+            try
+            {
+                CrossGeolocator.Current.PositionChanged -= CrossGeolocator_Current_PositionChanged;
+                CrossGeolocator.Current.PositionError -= CrossGeolocator_Current_PositionError;
+            }
+            catch
+            {
+            }
+        }
+
+
 
         private async void Handle_GPS(object sender, System.EventArgs e)
         {
@@ -50,16 +145,10 @@ namespace Crosshelper.Views
             LongitudeLabel.Text = Settings.CurrentLongitude.ToString();
             LatitudeLabel.Text = Settings.CurrentLatitude.ToString();
         }
-
+        //GeolocationHelper gh = new GeolocationHelper();
         void Handle_Clicked(object sender, System.EventArgs e)
         {
-            //Position p = Task.Run(GetCurrentLocationAsync).Result;
 
-            //Task.Yield();
-            //Settings.CurrentLatitude = p.Latitude;
-            //Settings.CurrentLongitude = p.Longitude;
-            //LongitudeLabel.Text = Settings.CurrentLongitude.ToString();
-            //LatitudeLabel.Text = Settings.CurrentLatitude.ToString();
         }
 
 
