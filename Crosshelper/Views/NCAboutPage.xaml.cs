b03128379f21010895e06d54294973988b01f713
William Liang <williamliang30@gmail.com>
2019-05-07 15:56:49 -0700

    mm

--- Crosshelper/Views/NCAboutPage.xaml.cs
+++ Crosshelper/Views/NCAboutPage.xaml.cs
@@ -1,24 +1,48 @@
-﻿using System;
-using System.Collections.Generic;
-
+﻿using Plugin.Messaging;
 using Xamarin.Forms;
+
 
 namespace Crosshelper.Views
 {
     public partial class NCAboutPage : ContentPage
     {
+        private string supportsEmail, emailTitle, emailBody;
+
+
         void Handle_Canceled(object sender, System.EventArgs e)
         {
             Navigation.PopToRootAsync(false);
         }
+
         public NCAboutPage()
         {
             InitializeComponent();
+
+            supportsEmail = "bo.chen@cycbis.com";
+            emailTitle = "[SupportsTicket]Feedback from Customer";
+            emailBody = emaileditor.Text;
+
+            SubmitBtn.Clicked += async (sender, e) =>
+            {
+                try
+                {
+                    var emailTask = CrossMessaging.Current.EmailMessenger;
+                    if (emailTask.CanSendEmail)
+                        emailTask.SendEmail(supportsEmail, emailTitle, emailBody);
+                    else
+                        await DisplayAlert("Error", "This device can't send emails", "OK");
+                }
+                catch
+                {
+                    await DisplayAlert("Error", "Unable to perform action", "OK");
+                }
+                //Navigation.PopAsync(false);
+            };
         }
 
         void Handle_Submit(object sender, System.EventArgs e)
         {
-            Navigation.PopAsync(false);
+
         }
     }
 }
