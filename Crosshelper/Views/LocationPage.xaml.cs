27488eb60da3941ba49dffff8b03a761ab05f9ac
William Liang <williamliang30@gmail.com>
2019-05-19 17:00:36 -0700

    o

--- Crosshelper/Views/LocationPage.xaml.cs
+++ Crosshelper/Views/LocationPage.xaml.cs
@@ -8,13 +8,80 @@ using Plugin.Geolocator.Abstractions;
 using Xamarin.Forms;
 using Xamarin.Essentials;
 using System.Linq;
+using Crosshelper.PlacesBar;
 
 namespace Crosshelper.Views
 {
     public partial class LocationPage : ContentPage
     {
+        string GooglePlacesApiKey = "AIzaSyBcxooWTNnHQJb10x0cgkijXn5fowDfUjE";//Keys.ApiKey;  // Replace this with your api key
 
         public LocationPage()
+        {
+            InitializeComponent();
+
+            search_bar.ApiKey = GooglePlacesApiKey;
+            search_bar.Type = PlaceType.Address;
+            search_bar.Components = new Components("country:au|country:nz"); // Restrict results to Australia and New Zealand
+            search_bar.PlacesRetrieved += Search_Bar_PlacesRetrieved;
+            search_bar.TextChanged += Search_Bar_TextChanged;
+            search_bar.MinimumSearchText = 2;
+            results_list.ItemSelected += Results_List_ItemSelected;
+        }
+
+        void Search_Bar_PlacesRetrieved(object sender, AutoCompleteResult result)
+        {
+            results_list.ItemsSource = result.AutoCompletePlaces;
+            spinner.IsRunning = false;
+            spinner.IsVisible = false;
+
+            if (result.AutoCompletePlaces != null && result.AutoCompletePlaces.Count > 0)
+                results_list.IsVisible = true;
+        }
+
+        void Search_Bar_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (!string.IsNullOrEmpty(e.NewTextValue))
+            {
+                results_list.IsVisible = false;
+                spinner.IsVisible = true;
+                spinner.IsRunning = true;
+            }
+            else
+            {
+                results_list.IsVisible = true;
+                spinner.IsRunning = false;
+                spinner.IsVisible = false;
+            }
+        }
+
+        async void Results_List_ItemSelected(object sender, SelectedItemChangedEventArgs e)
+        {
+            if (e.SelectedItem == null)
+                return;
+
+            var prediction = (AutoCompletePrediction)e.SelectedItem;
+            results_list.SelectedItem = null;
+
+            var place = await Places.GetPlace(prediction.Place_ID, GooglePlacesApiKey);
+
+            if (place != null)
+                await DisplayAlert(
+                    place.Name, string.Format("Lat: {0}\nLon: {1}", place.Latitude, place.Longitude), "OK");
+        }
+
+        void Handle_Canceled(object sender, EventArgs e)
+        {
+            Navigation.PopModalAsync();
+        }
+
+
+
+
+
+
+
+        /*public LocationPage()
         {
             InitializeComponent();
 
@@ -192,13 +259,10 @@ namespace Crosshelper.Views
                     //position.Altitude, position.AltitudeAccuracy, position.Accuracy, position.Heading, position.Speed);
             //Debug.WriteLine(output);
             return position;
-                    */
+                    
         }
 
-        void Handle_Canceled(object sender, EventArgs e)
-        {
-            Navigation.PopModalAsync();
-        }
+
 
         private async void Handle_GPS(object sender, EventArgs e)
         {
@@ -230,7 +294,13 @@ namespace Crosshelper.Views
                 Debug.WriteLine("Unable to get address: " + ex);
             }
             //Navigation.PopAsync();
-        }
+        }*/
+
+
+
+
+
+
 
     }
 }
