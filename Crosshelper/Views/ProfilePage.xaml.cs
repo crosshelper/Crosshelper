0a0a7febafd60f605597e0f45272d4d7bf9d5f7d
WL-CHelper <william.liang@crosshelper.com>
2019-05-20 20:03:24 -0700

    234

--- Crosshelper/Views/ProfilePage.xaml.cs
+++ Crosshelper/Views/ProfilePage.xaml.cs
@@ -1,5 +1,9 @@
 ﻿using System;
 using System.Threading.Tasks;
+using Amazon;
+using Amazon.CognitoIdentity;
+using Amazon.S3;
+using Amazon.S3.Transfer;
 using Crosshelper.Helpers;
 using Crosshelper.Models;
 using Plugin.Media;
@@ -14,11 +18,16 @@ namespace Crosshelper.Views
     public partial class ProfilePage : ContentPage
     {
         // 初始化 Amazon Cognito 凭证提供程序
-        /*CognitoAWSCredentials credentials = new CognitoAWSCredentials(
+        CognitoAWSCredentials credentials = new CognitoAWSCredentials(
             "us-east-1:220800bd-8233-4785-b80e-7f440926f503", // 身份池 ID
             RegionEndpoint.USEast1 // 区域
         );
-        */
+
+        private string filename = Settings.ChatID + DateTime.Now.ToShortDateString();
+        private const string bucketName = "image.cycbis.com/UserProfileIcon";
+        // Specify your bucket region (an example region is shown).
+        private static readonly RegionEndpoint bucketRegion = RegionEndpoint.USWest1;
+        private static IAmazonS3 s3Client = new AmazonS3Client(bucketRegion);
 
         void Handle_Saved(object sender, System.EventArgs e)
         {
@@ -91,7 +100,21 @@ namespace Crosshelper.Views
                  });
 
                 if (file == null)
+                {
                     return;
+                }
+                else
+                {
+                    var fileTransferUtility =
+                    new TransferUtility(s3Client);
+
+                    //await fileTransferUtility.UploadAsync(file.GetStream(), bucketName,"123");
+
+                    //await fileTransferUtility.UploadAsync(filename + file.AlbumPath, bucketName);
+
+                    await fileTransferUtility.UploadAsync(filename + file.Path, bucketName);
+
+                }
             }
         }
         ////check Permisson method
@@ -114,6 +137,7 @@ namespace Crosshelper.Views
         public string LastName { get; set; }
         public string PhoneNumber { get; set; }
         UserInfoHelper uih = new UserInfoHelper();
+
         public ProfilePage(User user)
         {
             InitializeComponent();
