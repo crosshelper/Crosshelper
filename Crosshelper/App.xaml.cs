567819fd260fbf0316eaefadda930f4b3ad1b184
William Liang <williamliang30@gmail.com>
2019-05-21 01:45:08 -0700

    11222

--- Crosshelper/App.xaml.cs
+++ Crosshelper/App.xaml.cs
@@ -1,24 +1,17 @@
-using System;
-using System.Diagnostics;
-using System.Threading.Tasks;
 using Crosshelper.Views;
-using Plugin.Geolocator;
-using Plugin.Geolocator.Abstractions;
 using SendBird;
-using Xamarin.Forms;
+using Stripe;
 using Xamarin.Forms.Xaml;
-using Crosshelper.Helpers;
 
 [assembly: XamlCompilation(XamlCompilationOptions.Compile)]
 namespace Crosshelper
 {
-    public partial class App : Application
+    public partial class App : Xamarin.Forms.Application
     {
 
         public App()
         {
             InitializeComponent();
-            SendBirdClient.Init(Crosshelper.Properties.Resources.APP_ID);
             //MainPage = new NavigationPage((Page)Activator.CreateInstance(typeof(SignInPage))) {
             //BarBackgroundColor = Color.FromHex("#FF4E18"),
             //BarTextColor = Color.White };
@@ -27,6 +20,8 @@ namespace Crosshelper
 
         protected override void OnStart()
         {
+            SendBirdClient.Init(Crosshelper.Properties.Resources.APP_ID);
+            StripeConfiguration.SetApiKey("sk_live_XXXXXXXXXXXXXXX");
             // Handle when your app starts
         }
 
--- Crosshelper/Views/QuotePage.xaml.cs
+++ Crosshelper/Views/QuotePage.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Windows.Input;
+using Stripe;
 using Xamarin.Forms;
 
 namespace Crosshelper.Views
@@ -25,6 +26,45 @@ namespace Crosshelper.Views
 
         void Handle_Accept(object sender, System.EventArgs e)
         {
+            var options = new TokenCreateOptions
+            {
+                Card = new CreditCardOptions
+                {
+                    Number = "8764534657658792",
+                    ExpYear = Convert.ToInt64("2022"),
+                    ExpMonth = Convert.ToInt64("04"),
+                    Cvc = "684",
+                    Currency = "eur"
+                }
+            };
+
+
+            var service = new TokenService();
+
+            Token stripeToken = service.Create(options);
+
+            var token = stripeToken;
+
+            var checkToken = token.Id;
+
+            var options2 = new ChargeCreateOptions
+            {
+                Amount = Convert.ToInt64("20.89"),//valor),
+                Currency = "eur",
+                Description = "Charge for ",
+                SourceId = checkToken
+            };
+            var service1 = new ChargeService();
+
+            try
+            {
+                Charge charge = service1.Create(options2);
+            }
+            catch (Exception ex)
+            {
+                DisplayAlert("teste", ex.Message, "ok");
+            }
+
             Navigation.PopAsync();
         }
 
