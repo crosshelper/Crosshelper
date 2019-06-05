4f42963bba36672a112afea3648f54b0f460a7e3
William Liang <williamliang30@gmail.com>
2019-06-04 22:06:33 -0700

    chatdone

---
+++ Crosshelper.iOS/Renderers/ExtendedListViewRenderer.cs
@@ -0,0 +1,26 @@
+﻿using Crosshelper.Controls;
+using Crosshelper.iOS.Renderers;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.iOS;
+
+[assembly: ExportRenderer(typeof(ExtendedListView), typeof(ExtendedListViewRenderer))]
+namespace Crosshelper.iOS.Renderers
+{
+    public class ExtendedListViewRenderer : ListViewRenderer
+    {
+        protected override void OnElementChanged(ElementChangedEventArgs<ListView> e)
+        {
+            base.OnElementChanged(e);
+
+            if (e.NewElement != null)
+            {
+                if (Control != null)
+                {
+                    Control.AllowsSelection = false;
+                    Control.AlwaysBounceVertical = false;
+                    Control.Bounces = false;
+                }
+            }
+        }
+    }
+}
