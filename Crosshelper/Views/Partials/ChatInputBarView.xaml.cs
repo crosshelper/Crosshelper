8e7195d1ef822ba74c3bca172ae10d1f01b1bc16
william <william.liang@crosshelper.com>
2019-06-06 19:08:25 -0700

    e1e12e

--- Crosshelper/Views/Partials/ChatInputBarView.xaml.cs
+++ Crosshelper/Views/Partials/ChatInputBarView.xaml.cs
@@ -20,6 +20,7 @@ namespace Crosshelper.Views.Partials
         {
             (this.Parent.Parent.BindingContext as ChatTestViewModel).OnSendCommand.Execute(null);
             chatTextInput.Focus();
+            chatTextInput.Text = "";
         }
 
         public void UnFocusEntry(){
