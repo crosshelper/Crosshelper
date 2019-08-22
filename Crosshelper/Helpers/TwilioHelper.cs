fbeb0356ad2768bd9cb56af55fb85d2346fd1411
william <william.liang@crosshelper.com>
2019-08-22 12:44:10 -0700

    1

---
+++ Crosshelper/Helpers/TwilioHelper.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Text.RegularExpressions;
+using PhoneNumbers;
+
+namespace Crosshelper.Helpers
+{
+    public class TwilioHelper
+    {
+        public bool IsValidPhone(string Phone)
+        {
+            try
+            {
+                if (string.IsNullOrEmpty(Phone))
+                    return false;
+                string pattern = @"\(?\d{3}\)?-? *\d{3}-? *-?\d{4}";
+                var r = new Regex(pattern, RegexOptions.IgnoreCase);
+                return r.IsMatch(Phone);
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+        public bool IsValidE164(string Phone, string CountryCode)
+        {
+            try
+            {
+                if (string.IsNullOrEmpty(Phone))
+                    return false;
+                var phoneUtil = PhoneNumberUtil.GetInstance();
+                var numberProto = phoneUtil.Parse(Phone, CountryCode);
+                var formattedPhone = phoneUtil.Format(numberProto, PhoneNumberFormat.INTERNATIONAL);
+                var r = phoneUtil.IsValidNumber(numberProto);
+                return r;
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+    }
+}
